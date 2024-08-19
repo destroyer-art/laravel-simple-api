bbf9503f1d34a8cf394633cf147e18dfba703c1b
destroyer-art <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>
2024-08-19 13:20:19 +0200

    feat/ add controller and route

--- app/Http/Controllers/SubmissionController.php
+++ app/Http/Controllers/SubmissionController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Jobs\ProcessSubmission;
+use Validator;
 
 class SubmissionController extends Controller
 {
-    //
+    public function submit(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => "required|string",
+            'email' => 'required|email',
+            'message' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json()(['error' => $validator->errors()], 422);
+        }
+
+        ProcessSubmission::dispatch(($request->all()));
+        return response()->json(['success' => "Submission is being Processed0"], 202);
+
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\SubmissionController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/submit', [SubmissionController::class, 'submit']);
