f3555a6930f67bb913948e33193200402322234d
destroyer-art <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>
2024-08-19 12:57:13 +0200

    feat/ db setup

--- database/migrations/2024_08_19_105453_create_submissions_table.php
+++ database/migrations/2024_08_19_105453_create_submissions_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('submissions', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->string('email');
+            $table->text('message');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('submissions');
     }
 };
