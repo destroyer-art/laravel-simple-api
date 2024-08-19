c6fe403a76e021bbb41e63853c6fb819624d3c1a
destroyer-art <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>
2024-08-19 13:06:00 +0200

    feat/ add job

---
+++ app/Listeners/LogSubmissionSaved.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LogSubmissionSaved
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        //
+    }
+}
